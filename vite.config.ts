ce1587fb8de8f19bb51ae510651e5ec9169f521b
Cauã Pinheiro <cauaspinheiro@gmail.com>
2021-11-01 12:51:50 -0300

    fix(vite): arruma configuração de ambiente do vite
    JIRA: TCC-172

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-declare const VITE_VERCEL_ENV: string
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), tsconfigPaths()],
@@ -12,7 +10,7 @@ export default defineConfig({
     __ENV__: JSON.stringify(
       process.env.NODE_ENV !== 'production'
         ? 'development'
-        : VITE_VERCEL_ENV || 'production'
+        : process.env.VITE_VERCEL_ENV || 'production'
     ),
   },
 })
